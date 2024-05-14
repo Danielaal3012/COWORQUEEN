9ad439a6ef0486dccad2788780767a9044ab0775
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-05-14 15:41:44 +0100

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -16,14 +16,14 @@ const Layout = ({ children }) => {
 
   if (isDesktop) {
     return (
-      <div className="relative w-full bg-transparent h-dvh">
-        <nav className="flex px-4 h-14 bg-secondary/75 ">
-          
-          <section className="flex items-center justify-between w-full">
+      <div className="relative flex flex-col w-full bg-transparent h-dvh">
+        <nav className="flex items-center justify-between w-full h-14 bg-secondary/75">
+          <section className="w-1/5 px-4 bg-slate-200">
             <Link to="/">
               <img src={Logo} alt="Logo" className="h-10" />
             </Link>
-            <section className="flex items-center gap-4">
+            </section>
+            <section className="flex items-center px-4">
               <Link to="/profile">
               <Avatar className="h-11 w-11">
                 <AvatarFallback>
@@ -33,9 +33,18 @@ const Layout = ({ children }) => {
               </Avatar>
               </Link>
               </section>
-            </section>
         </nav>
-        {children}
+
+        <div className="flex flex-row h-full py-4 gap-x-4">
+          <div className="flex flex-col w-1/5 h-full bg-slate-200">
+            menu lateral admin
+          </div>
+          <div className="flex flex-row w-4/5 h-full p-4 mr-4 rounded-lg bg-secondary/15">
+            {children}
+          </div>
+
+        </div>
+
         </div>
     );
   } else {
