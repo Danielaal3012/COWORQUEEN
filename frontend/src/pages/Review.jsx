76b8ae6cb4208c4fb6399337de694f57cf4ebf29
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-05-15 19:10:41 +0100

    Updated Review.jsx, /login, handleFileUpload

--- frontend/src/pages/Review.jsx
+++ frontend/src/pages/Review.jsx
@@ -1,62 +1,64 @@
-import React, { useState, useContext } from 'react';
-import { AuthContext } from '../auth/auth-context.jsx'; 
-import { toast } from 'react-toastify'; 
-import { useNavigate } from 'react-router-dom';
-import { Label } from '@/components/UI/label.jsx';
-import { Textarea } from '@/components/UI/textarea.jsx';
-import { Button } from '@/components/UI/button.jsx';
-import Rating from 'react-rating'; 
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../auth/auth-context.jsx";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { Label } from "@/components/UI/label.jsx";
+import { Textarea } from "@/components/UI/textarea.jsx";
+import { Button } from "@/components/UI/button.jsx";
+import Rating from "react-rating";
 
 function ReviewUseForm() {
   const { authState } = useContext(AuthContext);
+
   const [formData, setFormData] = useState({
     description: "",
     rate: 0,
     roomId: "",
     reservationId: "",
   });
+
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
-    ...formData,
+      ...formData,
       [name]: value,
     });
   };
 
   const handleRateChange = (rating) => {
     setFormData({
-     ...formData,
+      ...formData,
       rate: rating,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.description ||!formData.rate) {
+    if (!formData.description || !formData.rate) {
       toast.error("Revisa los campos obligatorios");
       return;
     }
     try {
       const response = await fetch("http://localhost:3000/review/add", {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-           Authorization: authState.token,
+          "Content-Type": "application/json",
+          Authorization: authState.token,
         },
         body: JSON.stringify({
           description: formData.description,
-          rate: formData.rate, 
-          roomId: room.id, 
-          reservationId: reservation.id, 
+          rate: formData.rate,
+          roomId: room.id,
+          reservationId: reservation.id,
         }),
       });
-      if (response.status!== 201) {
+      if (response.status !== 201) {
         toast.error("Error en los datos introducidos");
       } else {
         toast.success("Review hecha exitosamente");
-        navigate("/rooms"); 
+        navigate("/rooms");
       }
     } catch (error) {
       toast.error("Error al enviar la solicitud");
@@ -64,36 +66,36 @@ function ReviewUseForm() {
   };
 
   return (
-    <div>
-      <h1>Agregar una revisión</h1>
-      <form onSubmit={handleSubmit}>
-        <Label>
-          Descripción:
-          <Textarea
-            type="text"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-          />
-        </Label>
-        <br />
-        <Label>
-          Calificación:
-          <Rating
-            initialRating={formData.rate}
-            onChange={handleRateChange}
-            emptySymbol="★"
-            fullSymbol="★★★★★"
-            fractions={true} 
-            max={5} 
-          />
-        </Label>
-        <br />
-        <Button type="submit">Enviar revisión</Button>
-      </form>
+    <div className="flex flex-col w-full px-4 md:px-0">
+      <h2>Agregar una revisión</h2>
+      <div className="flex flex-col w-full gap-y-4">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col my-4 w-fulljustify-normal gap-x-4">
+            <Label>Descripción:</Label>
+            <Textarea
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="flex items-center my-4 gap-x-4">
+            <Label>Calificación:</Label>
+            <Rating
+              initialRating={formData.rate}
+              onChange={handleRateChange}
+              // emptySymbol="★"
+              // fullSymbol="★★★★★"
+              max={5}
+            />
+          </div>
+
+          <Button type="submit" className="w-full">Enviar revisión</Button>
+        </form>
+      </div>
     </div>
   );
 }
 
 export default ReviewUseForm;
-
