459cb71a5e0a3eb4bc73c3108979278a478da269
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-05-28 17:36:46 +0100

    Update MyReservations.jsx

--- frontend/src/pages/reservations/MyReservations.jsx
+++ frontend/src/pages/reservations/MyReservations.jsx
@@ -33,61 +33,85 @@ const MyReservations = () => {
         console.log(nextReservations)
 
     return (
-        <div className='p-4'>
-            <h2>Próximas reservas</h2>
-            <ul className='flex flex-col justify-start gap-2 md:flex-row md:flex-wrap'>
-                {nextReservations.map((reservation) => (
-                <figure
-                key={reservation.id}
-                className="relative px-1 overflow-hidden border rounded-lg md:w-[30%] md:py-1 h-1/2 border-ring bg-vanilla/35 hover:bg-opacity-5"
-                >
-                <Link to={`/reservation/${reservation.id}`} className='flex flex-row items-center h-full gap-x-2'>
-                  <img
-                    src={host + "/uploads/rooms/" + reservation?.roomId + "/" + reservation?.roomCover}
-                    alt={reservation.roomName}
-                    className="w-1/3 md:max-h-[80px] rounded-md md:w-auto aspect-video"
-                    onError={(e) => {
-                      e.target.onerror = null; 
-                      e.target.src='/defaultSpace.jpg'
-                    }}
-                  />
-                  <div className='h-full'>
-                    <h3 className='text-xl font-bold'>{reservation?.roomName}</h3>
-                    <p>{formatDate(reservation?.reservationDateBeg)}</p>
-                    <p className='text-sm'>{formatTime(reservation?.reservationDateBeg)} - {formatTime(reservation?.reservationDateEnd)}</p>
-                  </div>
-</Link>
-</figure>      
-                ))}
-            </ul>
-            <h2 className='my-1'>Reservas anteriores</h2>
-            <ul className='flex flex-col justify-start gap-2 md:flex-row md:flex-wrap'>
-                {pastReservations.map((reservation) => (
-                                    <figure
-                                    key={reservation.id}
-                                    className="relative px-1 overflow-hidden border rounded-lg md:w-[30%] md:py-1 h-1/2 border-ring bg-vanilla/35 hover:bg-opacity-5"
-                                  >
-                                    <Link to={`/reservation/${reservation.id}/review`} className='flex flex-row items-center h-full gap-x-2'>
-                                      <img
-                                        src={host + "/uploads/rooms/" + reservation?.roomId + "/" + reservation?.roomCover}
-                                        alt={reservation.roomName}
-                                        className="w-1/3 md:max-h-[80px] rounded-md md:w-auto aspect-video"
-                                        onError={(e) => {
-                                          e.target.onerror = null; 
-                                          e.target.src='/defaultSpace.jpg'
-                                        }}
-                                      />
-                                      <div className='h-full '>
-                                        <h3 className='text-xl font-bold'>{reservation?.roomName}</h3>
-                                        <p>{formatSmallDate(reservation?.reservationDateBeg)}</p>
-                                        <p className='text-sm'>{formatTime(reservation?.reservationDateBeg)} - {formatTime(reservation?.reservationDateEnd)}</p>
-                                      </div>
-                    </Link>
-                    </figure> 
-                ))}
-            </ul>
-        </div>
-    )
+      <div className="p-4">
+        <h2>Próximas reservas</h2>
+        <ul className="flex flex-col justify-start gap-2 md:flex-row md:flex-wrap">
+          {nextReservations.map((reservation) => (
+            <figure
+              key={reservation.id}
+              className="relative px-1 overflow-hidden border rounded-lg md:min-w-[275px] md:max-w-[275px] md:py-1 h-1/2 border-ring bg-vanilla/35 hover:bg-opacity-5"
+            >
+              <Link
+                to={`/reservation/${reservation.id}`}
+                className="flex flex-row items-center h-full gap-x-2"
+              >
+                <img
+                  src={
+                    host +
+                    "/uploads/rooms/" +
+                    reservation?.roomId +
+                    "/" +
+                    reservation?.roomCover
+                  }
+                  alt={reservation.roomName}
+                  className="w-1/3 md:max-h-[80px] rounded-md md:w-auto aspect-video"
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "/defaultSpace.jpg";
+                  }}
+                />
+                <div className="h-full">
+                  <h3 className="text-xl font-bold">{reservation?.roomName}</h3>
+                  <p>{formatSmallDate(reservation?.reservationDateBeg)}</p>
+                  <p className="text-sm">
+                    {formatTime(reservation?.reservationDateBeg)} -{" "}
+                    {formatTime(reservation?.reservationDateEnd)}
+                  </p>
+                </div>
+              </Link>
+            </figure>
+          ))}
+        </ul>
+        <h2 className="my-1">Reservas anteriores</h2>
+        <ul className="flex flex-col justify-start gap-2 md:flex-row md:flex-wrap">
+          {pastReservations.map((reservation) => (
+            <figure
+              key={reservation.id}
+              className="relative px-1 overflow-hidden border rounded-lg md:min-w-[275px] md:max-w-[275px] md:py-1 h-1/2 border-ring bg-vanilla/35 hover:bg-opacity-5"
+            >
+              <Link
+                to={`/reservation/${reservation.id}/review`}
+                className="flex flex-row items-center h-full gap-x-2"
+              >
+                <img
+                  src={
+                    host +
+                    "/uploads/rooms/" +
+                    reservation?.roomId +
+                    "/" +
+                    reservation?.roomCover
+                  }
+                  alt={reservation.roomName}
+                  className="w-1/3 md:max-h-[80px] rounded-md md:w-auto aspect-video"
+                  onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = "/defaultSpace.jpg";
+                  }}
+                />
+                <div className="h-full ">
+                  <h3 className="text-xl font-bold">{reservation?.roomName}</h3>
+                  <p>{formatSmallDate(reservation?.reservationDateBeg)}</p>
+                  <p className="text-sm">
+                    {formatTime(reservation?.reservationDateBeg)} -{" "}
+                    {formatTime(reservation?.reservationDateEnd)}
+                  </p>
+                </div>
+              </Link>
+            </figure>
+          ))}
+        </ul>
+      </div>
+    );
 
 }
 
