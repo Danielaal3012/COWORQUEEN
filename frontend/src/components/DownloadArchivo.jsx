a19995797888d0d4112fba203b7f8712e67f8a16
Daniela Alvarez <dcac3012@gmail.com>
2024-05-28 15:09:54 +0200

    up

--- frontend/src/components/DownloadArchivo.jsx
+++ frontend/src/components/DownloadArchivo.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const DownloadButton = ({ fileUrl, fileName }) => {
 
   const handleDownload = () => {
-    // Crear un elemento <a>
+    
     const link = document.createElement('a');
-    link.href = fileUrl; // Establecer la URL del archivo
-    link.download = fileName; // Establecer el nombre del archivo a descargar
+    link.href = fileUrl; 
+    link.download = fileName; 
     document.body.appendChild(link);
     link.click();
-    document.body.removeChild(link); // Eliminar el elemento <a> después de hacer clic
+    document.body.removeChild(link); 
   };
 
   return (
